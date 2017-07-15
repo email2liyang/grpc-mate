bfc3532800137f931fd38b6a7185c025ea520f30
Ivan Li <ivan.li@practiceinsight.io>
2017-07-15 15:18:26 +0800

    add es transport

--- elasticsearch-service/src/main/java/io/datanerd/es/guice/Constants.java
+++ elasticsearch-service/src/main/java/io/datanerd/es/guice/Constants.java
@@ -5,6 +5,11 @@ package io.datanerd.es.guice;
  */
 public interface Constants {
 
+  //Config part
   String CONFIG_PROP_FILE_PATH = "config_prop_file_path";
   String CONFIG_PROP_FILE_PATH_DEFAULT = "/etc/config/data_nerd.properties";
+  //ES part
+  String CONFIG_ES_CLUSTER_HOST = "es_cluster_host";
+  String CONFIG_ES_CLUSTER_PORT = "es_cluster_port";
+  String CONFIG_ES_CLUSTER_NAME = "es_cluster_name";
 }
--- elasticsearch-service/src/main/java/io/datanerd/es/guice/ElasticSearchModule.java
+++ elasticsearch-service/src/main/java/io/datanerd/es/guice/ElasticSearchModule.java
@@ -4,9 +4,11 @@ import com.google.inject.AbstractModule;
 import com.google.inject.Singleton;
 
 import org.apache.commons.configuration2.Configuration;
+import org.elasticsearch.client.transport.TransportClient;
 
 /**
  * Main Guice Config Module
+ *
  * @author email2liyang@gmail.com
  */
 public class ElasticSearchModule extends AbstractModule {
@@ -14,5 +16,6 @@ public class ElasticSearchModule extends AbstractModule {
   @Override
   protected void configure() {
     bind(Configuration.class).toProvider(ConfigurationProvider.class).in(Singleton.class);
+    bind(TransportClient.class).toProvider(TransportClientProvider.class).in(Singleton.class);
   }
 }
