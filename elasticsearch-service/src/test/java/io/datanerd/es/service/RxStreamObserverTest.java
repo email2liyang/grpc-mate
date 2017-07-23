49ce3702a9d846063f2c87158f7feaef279bc4b6
Ivan Li <ivan.li@practiceinsight.io>
2017-07-23 11:05:16 +0800

    add disposable assert

--- elasticsearch-service/src/test/java/io/datanerd/es/service/RxStreamObserverTest.java
+++ elasticsearch-service/src/test/java/io/datanerd/es/service/RxStreamObserverTest.java
@@ -8,6 +8,7 @@ import java.util.Set;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.IntStream;
 
+import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 import io.reactivex.subjects.PublishSubject;
 
@@ -21,17 +22,17 @@ public class RxStreamObserverTest {
     Set<Integer> resultsHolder = Sets.newConcurrentHashSet();
     Set<Throwable> exceptionsHolder = Sets.newConcurrentHashSet();
     AtomicBoolean complete = new AtomicBoolean(false);
-    publishSubject
+    Disposable disposable = publishSubject
         .doOnNext(num->resultsHolder.add(num*2))
         .doOnError(t->exceptionsHolder.add(t))
         .doOnComplete(()->complete.compareAndSet(false,true))
         .subscribe();
-
+    assertThat(disposable.isDisposed()).isFalse();
     RxStreamObserver<Integer> rxStreamObserver = new RxStreamObserver<>(publishSubject);
 
     IntStream.range(1,5).forEach(num->rxStreamObserver.onNext(num));
     rxStreamObserver.onCompleted();
-
+    assertThat(disposable.isDisposed()).isTrue();
     assertThat(resultsHolder).containsExactly(2,4,6,8);
     assertThat(exceptionsHolder).isEmpty();
     assertThat(complete).isTrue();
@@ -43,22 +44,23 @@ public class RxStreamObserverTest {
     Set<Integer> resultsHolder = Sets.newConcurrentHashSet();
     Set<Throwable> exceptionsHolder = Sets.newConcurrentHashSet();
     AtomicBoolean complete = new AtomicBoolean(false);
-    publishSubject
+    Disposable disposable = publishSubject
         .observeOn(Schedulers.io())
         .doOnNext(num->resultsHolder.add(num*2))
         .doOnError(t->exceptionsHolder.add(t))
         .doOnComplete(()->complete.compareAndSet(false,true))
         .subscribe();
 
+    assertThat(disposable.isDisposed()).isFalse();
     RxStreamObserver<Integer> rxStreamObserver = new RxStreamObserver<>(publishSubject);
 
     IntStream.range(1,5).forEach(num->rxStreamObserver.onNext(num));
     rxStreamObserver.onCompleted();
-
     while(resultsHolder.size() < 4){
       Thread.sleep(500);
     }
 
+    assertThat(disposable.isDisposed()).isTrue();
     assertThat(resultsHolder).containsExactly(2,4,6,8);
     assertThat(exceptionsHolder).isEmpty();
     assertThat(complete).isTrue();
@@ -70,16 +72,17 @@ public class RxStreamObserverTest {
     Set<Integer> resultsHolder = Sets.newConcurrentHashSet();
     Set<Throwable> exceptionsHolder = Sets.newConcurrentHashSet();
     AtomicBoolean complete = new AtomicBoolean(false);
-    publishSubject
+    Disposable disposable = publishSubject
         .doOnNext(num->resultsHolder.add(num))
         .doOnError(t->exceptionsHolder.add(t))
         .doOnComplete(()->complete.compareAndSet(false,true))
         .subscribe();
-
+    assertThat(disposable.isDisposed()).isFalse();
     RxStreamObserver<Integer> rxStreamObserver = new RxStreamObserver<>(publishSubject);
 
     rxStreamObserver.onError(new IllegalStateException());
 
+    assertThat(disposable.isDisposed()).isTrue();
     assertThat(resultsHolder).isEmpty();
     assertThat(exceptionsHolder.size()).isEqualTo(1);
     assertThat(exceptionsHolder.iterator().next()).isInstanceOf(IllegalStateException.class);
