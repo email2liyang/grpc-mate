a79f20765bc772f41960475301446c57d8bfa753
Ivan Li <email2liyang@gmail.com>
2019-11-01 19:26:40 +0800

    add client stream servicer to server

--- grpc-mate-python/server/server.py
+++ grpc-mate-python/server/server.py
@@ -1,13 +1,14 @@
 import logging.config
 import yaml
 
-
 from concurrent import futures
 
 import grpc
 
 import grpc_mate.helloworld_pb2_grpc
 from service.greeter_servicer import GreeterServicer
+import grpc_mate.product_search_engine_pb2_grpc
+from service.product_update_servicer import ProductUpdateServiceServicer
 
 # Create a custom logger
 with open('logging.yaml', 'r') as f:
@@ -20,6 +21,8 @@ logger = logging.getLogger(__name__)
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     grpc_mate.helloworld_pb2_grpc.add_GreeterServicer_to_server(GreeterServicer(), server)
+    grpc_mate.product_search_engine_pb2_grpc.add_ProductUpdateServiceServicer_to_server(ProductUpdateServiceServicer(),
+                                                                                        server)
     server.add_insecure_port('[::]:8080')
     server.start()
     logger.debug('grpc server started at port 8080')
